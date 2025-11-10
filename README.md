c6dc35daa824cff400f68d2819b92baa52812100
Chai <apichai.freedom@gmail.com>
2025-11-11 00:16:05 +0700

    feat: remove unused code

--- README.md
+++ README.md
@@ -1,10 +1,1 @@
 # azure-service-bus-emu
-
-## Usage
-
-Run:
-```
-chmod +x start.sh
-
-./start.sh
-```
--- start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-podman network create public 2>/dev/null || echo "Network 'public' already exists"; podman-compose up -d
